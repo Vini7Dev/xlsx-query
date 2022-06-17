2e3ce9593b0dc72b035ef47d92934ddfff87e07a
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2022-06-17 10:08:06 -0300

    feat: starting server with express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import express from 'express';
+
+const SERVER_PORT = 3333;
+
+const server = express();
+
+server.get('/', (req, res) => res.json({ message: 'Success!' }));
+
+server.listen(SERVER_PORT, () => {
+  console.log(`===> Server started on port ${SERVER_PORT}`);
+});
