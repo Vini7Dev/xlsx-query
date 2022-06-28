ffe0b5e56186736b86fae173858064fe11bbc850
Vinicius7Codeby <viniciusgabriel.dev@gmail.com>
2022-06-28 18:27:23 -0300

    feat: add dotenv

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const SERVER_PORT = 3333;
+const SERVER_PORT = process.env.SERVER_PORT || 3333;
 
 app.listen(SERVER_PORT, () => {
   console.log(`===> Server started on port ${SERVER_PORT}`);
