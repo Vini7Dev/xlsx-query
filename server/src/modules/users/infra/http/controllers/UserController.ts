7a51526bf8b6ed693e1934f3e098cecf21bb05a9
Vinicius7Codeby <94920663+Vinicius7Codeby@users.noreply.github.com>
2022-06-28 17:56:39 -0300

    feat: add list users route

--- server/src/modules/users/infra/http/controllers/UserController.ts
+++ server/src/modules/users/infra/http/controllers/UserController.ts
@@ -2,8 +2,17 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
+import ListUsersService from '@modules/users/services/ListUsersService';
 
 class UserController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listUsersService = container.resolve(ListUsersService);
+
+    const usersList = await listUsersService.execute();
+
+    return res.json(usersList);
+  }
+
   public async create(req: Request, res: Response): Promise<Response> {
     const createUserService = container.resolve(CreateUserService);
 
